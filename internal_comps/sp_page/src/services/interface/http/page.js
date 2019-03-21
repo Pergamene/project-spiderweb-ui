2aaaababa81931b2be23982e895ff9e3ef3de750
Ryan Saunders <>
2019-03-21 08:34:30 -0600

    switching to hitting service instead

--- internal_comps/sp_page/src/services/interface/http/page.js
+++ internal_comps/sp_page/src/services/interface/http/page.js
@@ -1,9 +1,10 @@
-import { Http, defaultJsonHeaders } from 'interface-handler/src/http.js';
-import { baseEndpoints } from 'interface-handler/src/interface-state.js';
+import { Http } from 'interface-handler/src/http.js';
+import { BaseEndpoint } from 'interface-handler/src/interface-state.js';
 
 export const getPage = (pageId) => {
-  baseEndpoints.localBaseEndpoint = 'http://localhost:8782';
-  let headers = defaultJsonHeaders;
+  // @DEBUG: will need to move this to a global setter later.
+  BaseEndpoint.setLocalBaseEndpoint('http://localhost:8782');
+  let headers = Http.defaultJsonHeaders();
   headers['X-USER-ID'] = 'test@email.com';
   return new Promise((resolve, reject) => {
     Http.get(`api/page/${pageId}`, headers).then(data => {
