9d1509f0ec242195d4f983801664c3c05c7353a5
Pergamene <smartz373@gmail.com>
2019-05-10 19:59:44 -0600

    add detail edit

---
+++ internal_comps/sp_page/src/components/page-parts/edit/sp-page-detail-edit.js
@@ -0,0 +1,48 @@
+import { html, LitElement } from '@polymer/lit-element';
+import { SpPageStyles } from '../../sp-page-styles.js';
+import { LOCALE_EN } from '../../../../../sp_locale/src/entities/en.js';
+
+import '../../../../../sp_shared/src/components/sp-inline-text-field.js';
+
+export class SpPageDetailEdit extends LitElement {
+  render() {
+    debugger;
+    // @TODO: is doing the .label this way okay/correct?
+    return html`
+      ${SpPageStyles}
+      <style>
+        :host {
+          display: flex;
+        }
+      </style>
+
+      <div options-pane>
+        <sp-dropdown-btn revealed></sp-dropdown-btn>
+      </div>
+      <div page-pane>
+        <sp-inline-text-field
+          .valueContainer="${this.detail}"
+          .valueKey="${'title'}"
+          .label="${LOCALE_EN.SP_PAGE_EDIT.DETAIL_TITLE.LABEL}"></sp-inline-text-field>
+        <sp-inline-text-field
+          .valueContainer="${this.detail}"
+          .valueKey="${'summary'}"
+          .label="${LOCALE_EN.SP_PAGE_EDIT.DETAIL_SUMMARY.LABEL}"
+          multiline></sp-inline-text-field>
+        <sp-inline-text-field
+          .valueContainer="${this.detail}"
+          .valueKey="${'markdown'}"
+          .label="${LOCALE_EN.SP_PAGE_EDIT.DETAIL_MARKDOWN.LABEL}"
+          multiline></sp-inline-text-field>
+      </div>
+    `
+  }
+
+  static get properties() {
+    return {
+      detail: { type: Object },
+    }
+  }
+}
+
+window.customElements.define('sp-page-detail-edit', SpPageDetailEdit);
